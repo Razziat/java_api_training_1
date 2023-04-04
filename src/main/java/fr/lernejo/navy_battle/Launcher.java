f9fe0c33067b34de1d454c3ad92f0bd93f1df1b9
henri.thomas <henri.thomas@epita.fr>
2023-04-04 11:12:21 +0200

    Modify Launcher and merge the handler in Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,17 +1,38 @@
 package fr.lernejo.navy_battle;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 public class Launcher {
-
-    private static int port = 9876;
-    private static final int HTTP_OK_STATUS = 200;
-
-
     public static void main(String[] args) throws IOException {
-        // Create server
-        HttpServerSimple server = new HttpServerSimple(port, new HttpHandlerSimple());
-        // Start
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            port = 9999;
+        }
+        InetSocketAddress addr = new InetSocketAddress(port);
+        HttpServer server = HttpServer.create(addr, 0);
+        server.createContext("/ping", new Ping());
+        server.setExecutor(Executors.newSingleThreadExecutor());
         server.start();
+        System.out.println("Le serveur en ecoute sur le port: " + addr.getPort());
+    }
+}
+
+class Ping implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "OK";
+        exchange.sendResponseHeaders(200, response.length());
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
+            os.write(response.getBytes());
+        }
     }
 }
