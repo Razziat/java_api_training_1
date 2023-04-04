5a5554759458ff53f99c6b78672a1ff81e48de58
henri.thomas <henri.thomas@epita.fr>
2023-04-04 14:24:20 +0200

    fixe ping to ok

--- src/main/java/fr/lernejo/navy_battle/PingHandler.java
+++ src/main/java/fr/lernejo/navy_battle/PingHandler.java
@@ -10,7 +10,7 @@ public class PingHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String body = "Hello";
+        String body = "Ok";
         exchange.sendResponseHeaders(200, body.length());
         try (OutputStream os = exchange.getResponseBody()){
             os.write(body.getBytes());
