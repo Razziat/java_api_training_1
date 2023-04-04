e1cde8b180fe155832bdc1ad4d0210fbe28f3166
henri.thomas <henri.thomas@epita.fr>
2023-04-04 11:04:52 +0200

    first ping commit

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,17 @@
+package fr.lernejo.navy_battle;
+
+import java.io.IOException;
+
+public class Launcher {
+
+    private static int port = 9876;
+    private static final int HTTP_OK_STATUS = 200;
+
+
+    public static void main(String[] args) throws IOException {
+        // Create server
+        HttpServerSimple server = new HttpServerSimple(port, new HttpHandlerSimple());
+        // Start
+        server.start();
+    }
+}
